4c35a760c5a4e10a26dc53bb32c4ebbb7b0f6b1f
orz12 <ms_lyq@hotmail.com>
2024-07-24 00:31:22 +0800

    videowidth smaller

--- lib/pages/video/detail/view.dart
+++ lib/pages/video/detail/view.dart
@@ -958,7 +958,7 @@ class _VideoDetailPageState extends State<VideoDetailPage>
         );
       }
       final double videoWidth =
-          max(context.height / context.width * 1.04, 1 / 2) * context.width;
+          max(context.height / context.width * 1.03, 1 / 2) * context.width;
       final double videoHeight = videoWidth * 9 / 16;
       return Row(
         children: [
